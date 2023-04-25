d6069de1d71f151228c493a372365ab16f399071
samojeyinka <131479901+samojeyinka@users.noreply.github.com>
2023-04-25 07:07:37 -1200

    Update Data.jsx

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -5,7 +5,7 @@ import SocialM from './Socialmobile';
 
 const Data = () => {
   return (
-    <div className='home_data md:mt-[70px] max-w-[1020px]'>
+    <div className='home_data md:mt-[70px] max-w-[1020px] mr-[1rem]'>
       <h1 className="home_title flex items-center">Sam Ojeyinka
       <svg
                   width="36"
