167e98e0223da7b6943d851e34f448db8b9f9fbd
Ojeyinka Ifeoluwa Samuel <131479901+samojeyinka@users.noreply.github.com>
2023-05-01 11:35:28 -1200

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -13,6 +13,7 @@ import css from './css.png';
 import aboutH from './about-h.jpg';
 import proto from './proto.png';
 import coinPhase from './coinphase.png';
+import proimg1 from './proimg1.png;
 
 
 
@@ -34,6 +35,7 @@ export{
     aboutH,
     proto,
     coinPhase,
+    proimg1,
 
 
 }
